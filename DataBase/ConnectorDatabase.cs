f2c500e8aaa5b1d5f6d627bb5db6486d5e71da91
Alexander Andrenko <andrenkom@gmail.com>
2022-12-22 00:17:44 +0700

    Add project files.

---
+++ DataBase/ConnectorDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class ConnectorDatabase
+    {
+        #region Constructor
+        public ConnectorDatabase()
+        {
+
+        }
+        #endregion //Constructor
+
+        #region Properties
+
+
+        #endregion //Properties
+
+        #region Methods
+
+
+        #endregion //Methods
+
+    }
+}
